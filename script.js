26e37232cd4195777e70a53c265305bd7d8a0392
Tom Naccarato <tcarlnaccarato@gmail.com>
2023-04-19 16:30:46 +0100

    Add reset button and drag function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Declares constants and variables for HTML elements
 const CONTAINER = document.querySelector(".grid-container")
+const resetButton = document.getElementById("reset-button");
 var slider = document.getElementById("myRange");
 var squaresNumber = document.querySelector(".squaresnumber")
 
@@ -32,32 +33,55 @@ function makeGrid(size){
 function resetGrid(){
     const gridSquares = document.querySelectorAll(".gridSquare");
     for(let i = 0; i < gridSquares.length; i++){
-      gridSquares[i].remove();
-    }
-  }
-
-// Adds listeners to each square
-function addListeners(){
-    const gridSquares = document.querySelectorAll(".gridSquare");
-    for(let i = 0; i < gridSquares.length; i++){
-        // When mouse hovers over
-        gridSquares[i].addEventListener("mouseenter", function(){
-            gridSquares[i].classList.add("active");
-        });
-        // When mouse leaves square
-        gridSquares[i].addEventListener("mouseleave", function(){
-            gridSquares[i].classList.remove("active");
-        });
-        // When mouse is clicked
-        gridSquares[i].addEventListener("click", function(){
-            gridSquares[i].classList.toggle("coloured")
-        })
-    }
-    slider.oninput = function() {
-        document.querySelector(".squaresnumber").value = this.value;
-        resetGrid()
-        makeGrid(slider.value)
+        gridSquares[i].remove();
     }
 }
 
+// Adds listeners
+function addListeners() {
+    let mouseDown = false; // Variable to keep track of whether the mouse button is down
+    const gridSquares = document.querySelectorAll(".gridSquare");
+    for (let i = 0; i < gridSquares.length; i++) {
+        // When mouse hovers over
+        gridSquares[i].addEventListener("mouseenter", function () {
+            if (mouseDown) {
+                gridSquares[i].classList.add("active");
+                gridSquares[i].classList.add("coloured");
+            } else {
+                gridSquares[i].classList.add("active");
+            }
+    });
+        // When mouse leaves square
+        gridSquares[i].addEventListener("mouseleave", function () {
+            gridSquares[i].classList.remove("active");
+        });
+
+        // When mouse is clicked
+        gridSquares[i].addEventListener("mousedown", function () {
+            mouseDown = true; // Set mouseDown to true when mouse button is held down
+            gridSquares[i].classList.toggle("coloured");
+        });
+    }
+        // Add event listener to document for when mouse button is released
+        document.addEventListener("mouseup", function () {
+            mouseDown = false; // Set mouseDown to false when mouse button is released
+        });
+        // Adds a listener to the slider that changes the size of the grid
+        slider.oninput = function () {
+            document.querySelector(".squaresnumber").value = this.value;
+            resetGrid();
+            makeGrid(slider.value);
+        };
+        // Adds a listener to reset the grid on click
+        resetButton.addEventListener("click", function () {
+            resetGrid();
+            makeGrid(slider.value);
+        })
+}
+
+
+
+
+
+// Makes default grid of 4x4
 makeGrid(4)
